0d27b20ae30664bd14e37346decc3d759503a5c0
usuario <usuario@usuarios-MacBook-Pro.local>
2023-10-06 15:32:53 -0500

    controlled elements

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,12 @@
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -14,15 +23,47 @@ function logo() {
 }
 
 function Form() {
+
+  const [description, setdescription] = useState("")
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(e)
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
-    </div>
+      <select>
+        {Array.from({length: 20}, (_, i) => i + 1).map((num) => (<option value={num} key={num} >{num}</option>))}
+      </select>
+      <input type="text" placeholder="Item..." value={description} onChange={(e) => setdescription(e.target.value)} />
+      <button >add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id}/>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line-through"} : {}} >
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
